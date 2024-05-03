19d355ab68eb647ccb4d1c5dfbdef6b2d85e6db1
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-05-03 20:42:52 +0800

    [feat]: initial commit, set webPack and TypeScript

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve:
+    {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index-bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
