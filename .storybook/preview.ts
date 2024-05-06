1a8713d9f56f46e01d5070f5e257b5d4f713c29a
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-05-07 00:38:00 +0800

    [feat]: added styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from '@storybook/react'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '../src/styles/_boilerplate.css'
 
 const preview: Preview = {
   parameters: {
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import '../src/styles/_boilerplate.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import App from './App'
 import { makeServer } from './server/server'
 
